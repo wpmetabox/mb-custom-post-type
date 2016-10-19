d3b28ceb8ebcb82cf34b892541b4b688a236d7f6
Anh <anh@fitwp.com>
2016-10-19 14:00:47 +0700

    Fix register post types too late which makes the list of CPT doesn't load correctly.

--- inc/base/register.php
+++ inc/base/register.php
@@ -17,7 +17,7 @@ abstract class MB_CPT_Base_Register {
 	 */
 	public function __construct() {
 		// Register post types
-		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'init', array( $this, 'register_post_types' ), 5 );
 
 		// Change the output of post/bulk post updated messages
 		add_filter( 'post_updated_messages', array( $this, 'updated_message' ), 10, 1 );
