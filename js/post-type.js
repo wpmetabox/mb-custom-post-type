a9e2aa00aa0319ceb0ad2f48d6f932fac6bc4a8e
Anh Tran <anhtnt@elightup.com>
2019-09-06 15:29:59 +0700

    Reorganize functions in JS

--- js/post-type.js
+++ js/post-type.js
@@ -1,77 +1,30 @@
-(function ( $, angular, hljs, i18n ) {
+(function ( $, document, angular, hljs, i18n ) {
 	'use strict';
 
-	angular.module( 'mbPostType', [] ).controller( 'PostTypeController', [
-		'$scope', function ( $scope ) {
-			// Initialize labels
-			$scope.labels = {};
+	/**
+	 * Helper function to convert string to slug
+	 * @param str
+	 * @return string
+	 */
+	function stringToSlug( str ) {
+		// Trim the string
+		str = str.replace( /^\s+|\s+$/g, '' );
+		str = str.toLowerCase();
 
-			/**
-			 * Helper function to convert string to slug
-			 * @param str
-			 * @return string
-			 */
-			function stringToSlug( str ) {
-				// Trim the string
-				str = str.replace( /^\s+|\s+$/g, '' );
-				str = str.toLowerCase();
+		// Remove accents
+		var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;",
+			to = "aaaaeeeeiiiioooouuuunc------",
+			i, l;
 
-				// Remove accents
-				var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;",
-					to = "aaaaeeeeiiiioooouuuunc------",
-					i, l;
-
-				for ( i = 0, l = from.length; i < l; i ++ ) {
-					str = str.replace( new RegExp( from.charAt( i ), 'g' ), to.charAt( i ) );
-				}
-
-				str = str.replace( /[^a-z0-9 -]/g, '' ) // remove invalid chars
-						  .replace( /\s+/g, '-' ) // collapse whitespace and replace by -
-						  .replace( /-+/g, '-' ); // collapse dashes
-				return str;
-			}
-
-			// Update labels and slug when plural and singular name are updated
-			$scope.updateLabels = function () {
-				var params = [
-						'menu_name',
-						'name_admin_bar',
-						'all_items',
-						'add_new',
-						'add_new_item',
-						'edit_item',
-						'new_item',
-						'view_item',
-						'search_items',
-						'not_found',
-						'not_found_in_trash',
-						'parent_item_colon'
-					];
-				params.forEach( function( param ) {
-					$scope.labels[param] = i18n[param].replace( '%name%', $scope.labels.name ).replace( '%singular_name%', $scope.labels.singular_name );
-				} );
-
-				// Update slug, make sure it has <= 20 characters.
-				var slug = stringToSlug( $scope.labels.singular_name );
-				if ( slug.length <= 20 ) {
-					$scope.post_type = stringToSlug( slug );
-				} else {
-					slug = slug.substring( 0, 20 );
-					var lastChar = slug.substr( 19 );
-					if ( '-' === lastChar ) {
-						slug = slug.substring( 0, 19 );
-					}
-
-					$scope.post_type = slug;
-				}
-			};
+		for ( i = 0, l = from.length; i < l; i ++ ) {
+			str = str.replace( new RegExp( from.charAt( i ), 'g' ), to.charAt( i ) );
 		}
-	] );
 
-	// Bootstrap AngularJS app
-	angular.element( document ).ready( function () {
-		angular.bootstrap( document.getElementById( 'wpbody-content' ), ['mbPostType'] );
-	} );
+		str = str.replace( /[^a-z0-9 -]/g, '' ) // remove invalid chars
+			.replace( /\s+/g, '-' ) // collapse whitespace and replace by -
+			.replace( /-+/g, '-' ); // collapse dashes
+		return str;
+	}
 
 	function toggleSettings( btn, target ) {
 		var $target = $( target );
@@ -83,16 +36,10 @@
 
 	/**
 	 * Add/Remove active class for selected/unselected menu icon
-	 * @return void
 	 */
 	function activeMenu() {
-		var $menuIcons = $( 'input[type="radio"][name="args_menu_icon"]' );
-
-		$menuIcons.on( 'click', function () {
-			var $this = $( this );
-			$menuIcons.closest( '.icon-single' ).removeClass( 'active' );
-			$this.closest( '.icon-single' ).addClass( 'active' );
-			$this.prop( 'checked', true );
+		$( 'input[name="args_menu_icon"]' ).on( 'change', function () {
+			$( this ).closest( '.icon-single' ).addClass( 'active' ).siblings().removeClass( 'active' );
 		} );
 	}
 
@@ -113,8 +60,50 @@
 		clipboard.on('error', function() {
 			alert( i18n.manualCopy );
 		});
-
 	}
+
+	angular.module( 'mbPostType', [] ).controller( 'PostTypeController', [ '$scope', function ( $scope ) {
+		// Initialize labels
+		$scope.labels = {};
+
+		// Update labels and slug when plural and singular name are updated
+		$scope.updateLabels = function () {
+			var params = [
+				'menu_name',
+				'name_admin_bar',
+				'all_items',
+				'add_new',
+				'add_new_item',
+				'edit_item',
+				'new_item',
+				'view_item',
+				'search_items',
+				'not_found',
+				'not_found_in_trash',
+				'parent_item_colon'
+			];
+			params.forEach( function( param ) {
+				$scope.labels[param] = i18n[param].replace( '%name%', $scope.labels.name ).replace( '%singular_name%', $scope.labels.singular_name );
+			} );
+
+			// Update slug, make sure it has <= 20 characters.
+			var slug = stringToSlug( $scope.labels.singular_name );
+			if ( slug.length > 20 ) {
+				slug = slug.substring( 0, 20 );
+				var lastChar = slug.substr( 19 );
+				if ( '-' === lastChar ) {
+					slug = slug.substring( 0, 19 );
+				}
+			}
+
+			$scope.post_type = slug;
+		};
+	} ] );
+
+	// Bootstrap AngularJS app
+	angular.element( document ).ready( function () {
+		angular.bootstrap( document.getElementById( 'wpbody-content' ), ['mbPostType'] );
+	} );
 
 	// Run when document is ready
 	$( function () {
@@ -124,4 +113,4 @@
 		copyToClipboard();
 	} );
 	hljs.initHighlightingOnLoad();
-} )( jQuery, angular, hljs, MbCptLabels );
+} )( jQuery, document, angular, hljs, MbCptLabels );
--- js/taxonomy.js
+++ js/taxonomy.js
@@ -1,71 +1,31 @@
-( function ( $, angular, hljs, i18n ) {
+( function ( $, document, angular, hljs, i18n ) {
 	'use strict';
 
-	angular.module( 'mbTaxonomy', [] ).controller( 'TaxonomyController', [
-		'$scope', function ( $scope ) {
-			// Initialize labels
-			$scope.labels = {};
+	/**
+	 * Helper function to convert string to slug
+	 * @param str
+	 * @return string
+	 */
+	function stringToSlug( str ) {
+		// Trim the string
+		str = str.replace( /^\s+|\s+$/g, '' );
+		str = str.toLowerCase();
 
-			/**
-			 * Helper function to convert string to slug
-			 * @param str
-			 * @return string
-			 */
-			function stringToSlug( str ) {
-				// Trim the string
-				str = str.replace( /^\s+|\s+$/g, '' );
-				str = str.toLowerCase();
+		// Remove accents
+		var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;",
+			to = "aaaaeeeeiiiioooouuuunc------",
+			i, l;
 
-				// Remove accents
-				var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;",
-					to = "aaaaeeeeiiiioooouuuunc------",
-					i, l;
-
-				for ( i = 0, l = from.length; i < l; i ++ ) {
-					str = str.replace( new RegExp( from.charAt( i ), 'g' ), to.charAt( i ) );
-				}
-
-				str = str.replace( /[^a-z0-9 -]/g, '' ) // remove invalid chars
-					.replace( /\s+/g, '-' ) // collapse whitespace and replace by -
-					.replace( /-+/g, '-' ); // collapse dashes
-
-				return str;
-			}
-
-			// Update labels and slug when plural and singular name are updated
-			$scope.updateLabels = function () {
-				var params = [
-						'menu_name',
-						'all_items',
-						'edit_item',
-						'view_item',
-						'update_item',
-						'add_new_item',
-						'new_item_name',
-						'parent_item',
-						'parent_item_colon',
-						'search_items',
-						'popular_items',
-						'separate_items_with_commas',
-						'add_or_remove_items',
-						'choose_from_most_used',
-						'not_found'
-					],
-					i = params.length;
-				for ( ; i --; ) {
-					$scope.labels[params[i]] = i18n[params[i]].replace( '%name%', $scope.labels.name ).replace( '%singular_name%', $scope.labels.singular_name );
-				}
-
-				// Update slug
-				$scope.taxonomy = stringToSlug( $scope.labels.singular_name );
-			};
+		for ( i = 0, l = from.length; i < l; i ++ ) {
+			str = str.replace( new RegExp( from.charAt( i ), 'g' ), to.charAt( i ) );
 		}
-	] );
 
-	// Bootstrap AngularJS app
-	angular.element( document ).ready( function () {
-		angular.bootstrap( document.getElementById( 'wpbody-content' ), ['mbTaxonomy'] );
-	} );
+		str = str.replace( /[^a-z0-9 -]/g, '' ) // remove invalid chars
+			.replace( /\s+/g, '-' ) // collapse whitespace and replace by -
+			.replace( /-+/g, '-' ); // collapse dashes
+
+		return str;
+	}
 
 	function toggleSettings( btn, target ) {
 		var $target = $( target );
@@ -95,6 +55,45 @@
 
 	}
 
+	angular.module( 'mbTaxonomy', [] ).controller( 'TaxonomyController', [
+		'$scope', function ( $scope ) {
+			// Initialize labels
+			$scope.labels = {};
+
+			// Update labels and slug when plural and singular name are updated
+			$scope.updateLabels = function () {
+				var params = [
+					'menu_name',
+					'all_items',
+					'edit_item',
+					'view_item',
+					'update_item',
+					'add_new_item',
+					'new_item_name',
+					'parent_item',
+					'parent_item_colon',
+					'search_items',
+					'popular_items',
+					'separate_items_with_commas',
+					'add_or_remove_items',
+					'choose_from_most_used',
+					'not_found'
+				];
+				params.forEach( function( param ) {
+					$scope.labels[param] = i18n[param].replace( '%name%', $scope.labels.name ).replace( '%singular_name%', $scope.labels.singular_name );
+				} );
+
+				// Update slug
+				$scope.taxonomy = stringToSlug( $scope.labels.singular_name );
+			};
+		}
+	] );
+
+	// Bootstrap AngularJS app
+	angular.element( document ).ready( function () {
+		angular.bootstrap( document.getElementById( 'wpbody-content' ), ['mbTaxonomy'] );
+	} );
+
 	// Run when document is ready
 	$( function () {
 		toggleSettings( '#ct-toggle-labels', '#mb-ct-label-settings' );
@@ -102,4 +101,4 @@
 		copyToClipboard();
 	} );
 	hljs.initHighlightingOnLoad()
-} )( jQuery, angular, hljs, MbCptLabels );
+} )( jQuery, document, angular, hljs, MbCptLabels );
