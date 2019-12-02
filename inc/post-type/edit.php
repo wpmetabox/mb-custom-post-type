06b48d40126eddded6e2d1d3dea865a60c2c467e
hsimah <hamishblake+github@gmail.com>
2019-12-02 09:43:35 -0800

    improve filter hook name

--- inc/post-type/edit.php
+++ inc/post-type/edit.php
@@ -327,7 +327,7 @@ class MB_CPT_Post_Type_Edit extends MB_CPT_Base_Edit {
 			),
 		);
 
-		$advanced_fields = apply_filters( 'rwmb_advanced_field_settings', $advanced_fields, $label_prefix, $args_prefix );
+		$advanced_fields = apply_filters( 'mbcpt_advanced_fields', $advanced_fields, $label_prefix, $args_prefix );
 
 		$code_fields = array(
 			array(
