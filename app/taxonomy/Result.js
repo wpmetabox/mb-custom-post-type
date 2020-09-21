cfa771a378f90c21334324cfd9100f30e3ce5143
Anh Tran <anhtnt@elightup.com>
2020-09-21 11:44:51 +0700

    Style lại phần result

--- app/taxonomy/Result.js
+++ app/taxonomy/Result.js
@@ -15,10 +15,10 @@ const Result = () => {
 
 	return (
 		<div className="mb-cpt-result">
-			<div className="notice notice-success"><p>Copy the code and paste into your theme's <code>functions.php</code> file.</p></div>
+			<p>Copy the code and paste into your theme's <code>functions.php</code> file.</p>
 			<div className="mb-cpt-result__body">
 				<Highlight className="php">{PhpCode( state )}</Highlight>
-				<Clipboard className="components-button is-primary" title="Click to copy the code" data-clipboard-text={PhpCode( state )} onSuccess={copy}>{copied ? 'Copied' : 'Copy'}</Clipboard>
+				<Clipboard className="button" title="Click to copy the code" data-clipboard-text={PhpCode( state )} onSuccess={copy}>{copied ? 'Copied' : 'Copy'}</Clipboard>
 			</div>
 		</div>
 	);
