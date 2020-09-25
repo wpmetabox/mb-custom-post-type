46a5c808447aab7259196c590356859b59b23185
Anh Tran <anhtnt@elightup.com>
2020-09-25 14:31:46 +0700

    Update message for copy code

--- app/taxonomy/Result.js
+++ app/taxonomy/Result.js
@@ -6,23 +6,23 @@ const { useState, useContext } = wp.element;
 const { __ } = wp.i18n;
 
 const Result = () => {
-	const [copied, setCopied] = useState( false );
+	const [ copied, setCopied ] = useState( false );
 	const copy = () => {
 		setCopied( true );
 		setTimeout( () => setCopied( false ), 1000 );
-	}
+	};
 
-	const [state, setState] = useContext( PhpSettings );
+	const [ state, setState ] = useContext( PhpSettings );
 
 	return (
 		<div className="mb-cpt-result">
-			<p>{__( 'Copy the code and paste into your theme\'s <code>functions.php</code> file.', 'mb-custom-post-type' )}</p>
+			<p>{ __( 'Copy and paste the following code into your theme\'s functions.php file.', 'mb-custom-post-type' ) }</p>
 			<div className="mb-cpt-result__body">
-				<CodeMirror value={ PhpCode( state ) } options={ { mode: 'php', lineNumbers: true } }/>
-				<Clipboard className="button" title={__( 'Click to copy the code', 'mb-custom-post-type' )} data-clipboard-text={PhpCode( state )} onSuccess={copy}>{copied ? __( 'Copied', 'mb-custom-post-type' ) : __( 'Copy', 'mb-custom-post-type' )}</Clipboard>
+				<CodeMirror value={ PhpCode( state ) } options={ { mode: 'php', lineNumbers: true } } />
+				<Clipboard className="button" title={ __( 'Click to copy the code', 'mb-custom-post-type' ) } data-clipboard-text={ PhpCode( state ) } onSuccess={ copy }>{ copied ? __( 'Copied', 'mb-custom-post-type' ) : __( 'Copy', 'mb-custom-post-type' ) }</Clipboard>
 			</div>
 		</div>
 	);
-}
+};
 
 export default Result;
