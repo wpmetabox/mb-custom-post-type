35f0484f5acf8da4309dcacbac9d121aa96d6151
Anh Tran <anhtnt@elightup.com>
2020-09-29 00:04:16 +0700

    Update default settings

--- app/post-type/constants/DefaultSettings.js
+++ app/post-type/constants/DefaultSettings.js
@@ -1,60 +1,50 @@
-const BasicSettings = {
-	'name'         : '',
-	'slug'         : '',
-	'function_name': 'your_prefix_register_post_type',
-	'text_domain'  : 'your-textdomain',
-}
-
-const LabelSettings = {
-	labels: {
-		'singular_name'     : '',
-		'menu_name'         : '',
-		'name_admin_bar'    : '',
-		'add_new'           : '',
-		'add_new_item'      : '',
-		'new_item'          : '',
-		'edit_item'         : '',
-		'view_item'         : '',
-		'update_item'       : '',
-		'all_items'         : '',
-		'search_items'      : '',
-		'parent_item_colon' : '',
-		'not_found'         : '',
-		'not_found_in_trash': '',
-	}
-}
-
-export const SupportSettings = {
-	'supports': ['title', 'editor', 'thumbnail']
-}
-
-export const TaxonomySettings = {
-	'taxonomies': []
-}
-
-const AdvancedSettings = {
-	'description'        : '',
-	'public'             : true,
-	'exclude_from_search': false,
-	'publicly_queryable' : true,
-	'show_ui'            : true,
-	'show_in_nav_menus'  : true,
-	'show_in_admin_bar'  : true,
-	'show_in_rest'       : true,
-	'capability_type'    : 'post',
-	'hierarchical'       : false,
-	'has_archive'        : true,
-	'query_var'          : true,
-	'can_export'         : true,
-	'rewrite_no_front'   : false,
-}
-
 const DefaultSettings = {
-	...BasicSettings,
-	...LabelSettings,
-	...SupportSettings,
-	...TaxonomySettings,
-	...AdvancedSettings
-};
+	slug         : '',
+	function_name: 'your_prefix_register_post_type',
+	text_domain  : 'your-textdomain',
+
+	label         : '',
+	labels: {
+		singular_name     : '',
+		menu_name         : '',
+		name_admin_bar    : '',
+		add_new           : '',
+		add_new_item      : '',
+		new_item          : '',
+		edit_item         : '',
+		view_item         : '',
+		update_item       : '',
+		all_items         : '',
+		search_items      : '',
+		parent_item_colon : '',
+		not_found         : '',
+		not_found_in_trash: '',
+	},
+	description        : '',
+	public             : true,
+	hierarchical       : false,
+	exclude_from_search: false,
+	publicly_queryable : true,
+	show_ui            : true,
+	show_in_menu       : true,
+	show_in_nav_menus  : true,
+	show_in_admin_bar  : true,
+	show_in_rest       : true,
+	rest_base          : '',
+	menu_position      : null,
+	menu_icon          : '',
+	capability_type    : 'post',
+	capabilities       : [],
+	map_meta_cap       : false,
+	supports           : ['title', 'editor', 'thumbnail'],
+	taxonomies         : [],
+	has_archive        : true,
+	rewrite            : {
+		slug      : '',
+		with_front: false,
+	},
+	query_var          : '',
+	can_export         : true,
+}
 
 export default DefaultSettings;
