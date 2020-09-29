8e41ae0af587dd4c2797127061bbc3b1cc1aff7b
Anh Tran <anhtnt@elightup.com>
2020-09-29 10:15:34 +0700

    Update default settings

--- app/post-type/constants/Data.js
+++ app/post-type/constants/Data.js
@@ -2,8 +2,8 @@ import { Icons } from './Icons';
 const { __ } = wp.i18n;
 
 export const BasicDatas = [
-	{ type: 'text', name: 'name', label: __( 'Plural name', 'mb-custom-post-type' ), required: true },
-	{ type: 'text', name: 'singular_name', label: __( 'Singular name', 'mb-custom-post-type' ), required: true },
+	{ type: 'text', name: 'labels.name', label: __( 'Plural name', 'mb-custom-post-type' ), required: true },
+	{ type: 'text', name: 'labels.singular_name', label: __( 'Singular name', 'mb-custom-post-type' ), required: true },
 	{ type: 'text', name: 'slug', label: __( 'Slug', 'mb-custom-post-type' ), required: true, updateFrom: 'singular_name' },
 ];
 
@@ -13,18 +13,36 @@ export const CodeDatas = [
 ];
 
 export const LabelDatas = [
-	{ type: 'text', name: 'add_new', label: __( 'Add new', 'mb-custom-post-type' ), defaultValue: 'Add %singular_name%', updateFrom: 'singular_name' },
-	{ type: 'text', name: 'add_new_item', label: __( 'Add new item', 'mb-custom-post-type' ), defaultValue: 'Add new %singular_name%', updateFrom: 'singular_name' },
-	{ type: 'text', name: 'edit_item', label: __( 'Edit item', 'mb-custom-post-type' ), defaultValue: 'Edit %singular_name%', updateFrom: 'singular_name' },
-	{ type: 'text', name: 'new_item', label: __( 'New item', 'mb-custom-post-type' ), defaultValue: 'New %singular_name%', updateFrom: 'singular_name' },
-	{ type: 'text', name: 'view_item', label: __( 'View item', 'mb-custom-post-type' ), defaultValue: 'View %singular_name%', updateFrom: 'singular_name' },
-	{ type: 'text', name: 'view_items', label: __( 'View items', 'mb-custom-post-type' ), defaultValue: 'View %name%', updateFrom: 'name' },
-	{ type: 'text', name: 'search_items', label: __( 'Search items', 'mb-custom-post-type' ), defaultValue: 'Search %name%', updateFrom: 'name' },
-	{ type: 'text', name: 'not_found', label: __( 'Not found', 'mb-custom-post-type' ), defaultValue: 'No %name% found', updateFrom: 'name' },
-	{ type: 'text', name: 'not_found_in_trash', label: __( 'Not found in trash', 'mb-custom-post-type' ), defaultValue: 'No %name% found in Trash', updateFrom: 'name' },
-	{ type: 'text', name: 'parent_item_colon', label: __( 'Parent items', 'mb-custom-post-type' ), defaultValue: 'Parent %singular_name%', updateFrom: 'singular_name' },
-	{ type: 'text', name: 'all_items', label: __( 'All items', 'mb-custom-post-type' ), defaultValue: 'All %name%', updateFrom: 'name' },
-	{ type: 'text', name: 'menu_name', label: __( 'Menu name', 'mb-custom-post-type' ), defaultValue: '%name%', updateFrom: 'name' },
+	// Name
+	// Singular name
+	{ type: 'text', name: 'labels.add_new', label: __( 'Add new', 'mb-custom-post-type' ), defaultValue: __( 'Add New', 'mb-custom-post-type' ) },
+	{ type: 'text', name: 'labels.add_new_item', label: __( 'Add new item', 'mb-custom-post-type' ), defaultValue: __( 'Add new %singular_name%', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.edit_item', label: __( 'Edit item', 'mb-custom-post-type' ), defaultValue: __( 'Edit %singular_name%', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.new_item', label: __( 'New item', 'mb-custom-post-type' ), defaultValue: __( 'New %singular_name%', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.view_item', label: __( 'View item', 'mb-custom-post-type' ), defaultValue: __( 'View %singular_name%', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.view_items', label: __( 'View items', 'mb-custom-post-type' ), defaultValue: __( 'View %name%', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.search_items', label: __( 'Search items', 'mb-custom-post-type' ), defaultValue: __( 'Search %name%', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.not_found', label: __( 'Not found', 'mb-custom-post-type' ), defaultValue: __( 'No %name% found', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.not_found_in_trash', label: __( 'Not found in trash', 'mb-custom-post-type' ), defaultValue: __( 'No %name% found in Trash', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.parent_item_colon', label: __( 'Parent items', 'mb-custom-post-type' ), defaultValue: __( 'Parent %singular_name%:', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.all_items', label: __( 'All items', 'mb-custom-post-type' ), defaultValue: __( 'All %name%', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.archives', label: __( 'Nav menus archives', 'mb-custom-post-type' ), defaultValue: __( '%singular_name% Archives', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.attributes', label: __( 'Attributes meta box', 'mb-custom-post-type' ), defaultValue: __( '%singular_name% Attributes', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.insert_into_item', label: __( 'Media frame button', 'mb-custom-post-type' ), defaultValue: __( 'Insert into %singular_name%', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.uploaded_to_this_item', label: __( 'Media frame filter', 'mb-custom-post-type' ), defaultValue: __( 'Uploaded to this %singular_name%', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.featured_image', label: __( 'Featured image meta box', 'mb-custom-post-type' ), defaultValue: __( 'Featured image', 'mb-custom-post-type' ) },
+	{ type: 'text', name: 'labels.set_featured_image', label: __( 'Setting the featured image', 'mb-custom-post-type' ), defaultValue: __( 'Set featured image', 'mb-custom-post-type' ) },
+	{ type: 'text', name: 'labels.remove_featured_image', label: __( 'Removing the featured image', 'mb-custom-post-type' ), defaultValue: __( 'Remove featured image', 'mb-custom-post-type' ) },
+	{ type: 'text', name: 'labels.use_featured_image', label: __( 'Using a featured image in the media frame', 'mb-custom-post-type' ), defaultValue: __( 'Use as featured image', 'mb-custom-post-type' ) },
+	{ type: 'text', name: 'labels.menu_name', label: __( 'Menu name', 'mb-custom-post-type' ), defaultValue: __( '%name%', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.filter_items_list', label: __( 'Table views hidden heading', 'mb-custom-post-type' ), defaultValue: __( 'Filter %name% list', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.items_list_navigation', label: __( 'Table pagination hidden heading', 'mb-custom-post-type' ), defaultValue: __( '%name% list navigation', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.items_list', label: __( 'Table hidden heading', 'mb-custom-post-type' ), defaultValue: __( '%name% list', 'mb-custom-post-type' ), updateFrom: 'name' },
+	{ type: 'text', name: 'labels.item_published', label: __( 'Item published', 'mb-custom-post-type' ), defaultValue: __( '%singular_name% published', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.item_published_privately', label: __( 'Item published with private visibility', 'mb-custom-post-type' ), defaultValue: __( '%singular_name% published privately', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.item_reverted_to_draft', label: __( 'Item switched to draft', 'mb-custom-post-type' ), defaultValue: __( '%singular_name% reverted to draft.', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.item_scheduled', label: __( 'Item scheduled', 'mb-custom-post-type' ), defaultValue: __( '%singular_name% scheduled.', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
+	{ type: 'text', name: 'labels.item_updated', label: __( 'Item updated', 'mb-custom-post-type' ), defaultValue: __( '%singular_name% updated.', 'mb-custom-post-type' ), updateFrom: 'singular_name' },
 ];
 
 export const SupportDatas = [
@@ -87,23 +105,27 @@ const MenuPosition = [
 
 export const AdvancedDatas = [
 	{ type: 'textarea', name: 'description', label: __( 'Description', 'mb-custom-post-type' ), placeholder: __( 'A short descriptive summary of what the post type is', 'mb-custom-post-type' ) },
-	{ type: 'checkbox', name: 'public', label: __( 'Public?', 'mb-custom-post-type' ), description: __( 'Controls how the type is visible to authors and readers.', 'mb-custom-post-type' ), checked: true },
-	{ type: 'checkbox', name: 'exclude_from_search', label: __( 'Exclude from search?', 'mb-custom-post-type' ), description: __( 'Whether to exclude posts with this post type from frontend search results.', 'mb-custom-post-type' ), checked: false },
-	{ type: 'checkbox', name: 'publicly_queryable', label: __( 'Publicly queryable?', 'mb-custom-post-type' ), description: __( 'Whether queries can be performed on the frontend.', 'mb-custom-post-type' ), checked: true },
-	{ type: 'checkbox', name: 'show_ui', label: __( 'Show UI?', 'mb-custom-post-type' ), description: __( 'Whether to generate a default UI for managing this post type in the admin.', 'mb-custom-post-type' ), checked: true },
-	{ type: 'checkbox', name: 'show_in_nav_menus', label: __( 'Show in nav menus?', 'mb-custom-post-type' ), description: __( 'Whether post type is available for selection in navigation menus.', 'mb-custom-post-type' ), checked: true },
-	{ type: 'select', name: 'show_in_menu', label: __( 'Show in menu?', 'mb-custom-post-type' ), description: __( 'Where to show the post type in the admin menu. show_ui must be true.', 'mb-custom-post-type' ), values: ShowInMenuData, defaultValue: 0 },
-	{ type: 'checkbox', name: 'show_in_admin_bar', label: __( 'Show in admin bar?', 'mb-custom-post-type' ), description: __( 'Whether to make this post type available in the WordPress admin bar.', 'mb-custom-post-type' ), checked: true },
-	{ type: 'checkbox', name: 'show_in_rest', label: __( 'Show in rest?', 'mb-custom-post-type' ), description: __( 'Whether to add the post type in the REST API.', 'mb-custom-post-type' ), checked: true },
+	{ type: 'checkbox', name: 'public', label: __( 'Public', 'mb-custom-post-type' ), description: __( 'Controls how the type is visible to authors and readers.', 'mb-custom-post-type' ), checked: true },
+	{ type: 'checkbox', name: 'hierarchical', label: __( 'Hierarchical', 'mb-custom-post-type' ), description: __( 'Whether the post type is hierarchical.', 'mb-custom-post-type' ), checked: false },
+	{ type: 'checkbox', name: 'exclude_from_search', label: __( 'Exclude from search', 'mb-custom-post-type' ), description: __( 'Whether to exclude posts with this post type from frontend search results.', 'mb-custom-post-type' ), checked: false },
+	{ type: 'checkbox', name: 'publicly_queryable', label: __( 'Publicly queryable', 'mb-custom-post-type' ), description: __( 'Whether queries can be performed on the frontend.', 'mb-custom-post-type' ), checked: true },
+	{ type: 'checkbox', name: 'show_ui', label: __( 'Show UI', 'mb-custom-post-type' ), description: __( 'Whether to generate a default UI for managing this post type in the admin.', 'mb-custom-post-type' ), checked: true },
+	{ type: 'select', name: 'show_in_menu', label: __( 'Show in menu', 'mb-custom-post-type' ), description: __( 'Where to show the post type in the admin menu. show_ui must be true.', 'mb-custom-post-type' ), values: ShowInMenuData, defaultValue: 0 },
+	{ type: 'checkbox', name: 'show_in_nav_menus', label: __( 'Show in nav menus', 'mb-custom-post-type' ), description: __( 'Whether post type is available for selection in navigation menus.', 'mb-custom-post-type' ), checked: true },
+	{ type: 'checkbox', name: 'show_in_admin_bar', label: __( 'Show in admin bar', 'mb-custom-post-type' ), description: __( 'Whether to make this post type available in the WordPress admin bar.', 'mb-custom-post-type' ), checked: true },
+	{ type: 'checkbox', name: 'show_in_rest', label: __( 'Show in rest', 'mb-custom-post-type' ), description: __( 'Whether to add the post type in the REST API.', 'mb-custom-post-type' ), checked: true },
 	{ type: 'text', name: 'rest_base', label: __( 'REST API base slug', 'mb-custom-post-type' ), description: __( 'Leave empty to use the post type slug.', 'mb-custom-post-type' ), placeholder: __( 'Slug to use in REST API URL', 'mb-custom-post-type' ) },
 	{ type: 'select', name: 'menu_position', label: __( 'Menu position after', 'mb-custom-post-type' ), values: MenuPosition, defaultValue: '' },
 	{ type: 'radio', name: 'menu_icon', label: __( 'Menu icon', 'mb-custom-post-type' ), values: Icons },
 	{ type: 'radio', name: 'capability_type', label: __( 'Capability type', 'mb-custom-post-type' ), description: __( 'The post type to use for checking read, edit, and delete capabilities.', 'mb-custom-post-type' ), values: CapabilityDatas, defaultValue: 'post' },
-	{ type: 'checkbox', name: 'hierarchical', label: __( 'Hierarchical?', 'mb-custom-post-type' ), description: __( 'Whether the post type is hierarchical.', 'mb-custom-post-type' ), checked: false },
-	{ type: 'checkbox', name: 'has_archive', label: __( 'Has archive?', 'mb-custom-post-type' ), description: __( 'Enables post type archives.', 'mb-custom-post-type' ), checked: true },
+	{ type: 'checkbox', name: 'map_meta_cap', label: __( 'Map meta cap', 'mb-custom-post-type' ), description: __( 'Whether to use the internal default meta capability handling.', 'mb-custom-post-type' ), checked: false },
+	// supports
+	// taxonomies
+	{ type: 'checkbox', name: 'has_archive', label: __( 'Has archive', 'mb-custom-post-type' ), description: __( 'Enables post type archives.', 'mb-custom-post-type' ), checked: true },
 	{ type: 'text', name: 'archive_slug', label: __( 'Custom archive slug', 'mb-custom-post-type' ), description: __( 'Default is the post type slug.', 'mb-custom-post-type' ) },
+	{ type: 'text', name: 'rewrite.slug', label: __( 'Custom rewrite slug', 'mb-custom-post-type' ), description: __( 'Leave empty to use the post type slug.', 'mb-custom-post-type' ) },
+	{ type: 'checkbox', name: 'rewrite.with_front', label: __( 'Prepended permalink structure', 'mb-custom-post-type' ), description: __( 'Whether the permastruct should be prepended with <code>WP_Rewrite::$front</code>', 'mb-custom-post-type' ), checked: true },
 	{ type: 'checkbox', name: 'query_var', label: __( 'Query var', 'mb-custom-post-type' ), description: __( 'Enables request the post via URL: example.com/?post_type=slug', 'mb-custom-post-type' ), checked: true },
-	{ type: 'checkbox', name: 'can_export', label: __( 'Can export?', 'mb-custom-post-type' ), description: __( 'Can this post type be exported?', 'mb-custom-post-type' ), checked: true },
-	{ type: 'text', name: 'rewrite_slug', label: __( 'Custom rewrite slug', 'mb-custom-post-type' ), description: __( 'Leave empty to use the post type slug.', 'mb-custom-post-type' ) },
-	{ type: 'checkbox', name: 'rewrite_no_front', label: __( 'No prepended permalink structure?', 'mb-custom-post-type' ), description: __( 'Do not prepend the permalink structure with the front base.', 'mb-custom-post-type' ), checked: false },
+	{ type: 'checkbox', name: 'can_export', label: __( 'Can export', 'mb-custom-post-type' ), description: __( 'Can this post type be exported', 'mb-custom-post-type' ), checked: true },
+	{ type: 'checkbox', name: 'delete_with_user', label: __( 'Delete with user', 'mb-custom-post-type' ), description: __( 'Whether to move posts to Trash when deleting a user', 'mb-custom-post-type' ), checked: true },
 ];
