43a6fbffd9fb9dd69094184a80a75a079fea5885
Anh Tran <anhtnt@elightup.com>
2020-09-30 17:03:41 +0700

    Update missing labels

--- app/controls/Input.js
+++ app/controls/Input.js
@@ -1,14 +1,36 @@
-const Input = ( { label, name, value, update, description = '', required = false } ) => (
-	<div className="mb-cpt-field">
-		<label className="mb-cpt-label" htmlFor={ name }>
-			{ label }
-			{ required && <span className="mb-cpt-required">*</span> }
-		</label>
-		<div className="mb-cpt-input">
-			<input type="text" required={ required } id={ name } data-name={ name } value={ value } onChange={ update } />
-			{ description && <div className="mb-cpt-description">{ description }</div> }
+const { useEffect } = wp.element;
+
+const Input = ( { label, name, value, update, description = '', required = false } ) => {
+	// Update the labels when loaded (run only once)
+	// After migration, some labels are missing and show placeholders %name%, %singular_name%
+	useEffect( () => {
+		if ( ![ 'labels.name', 'labels.singular_name' ].includes( name ) ) {
+			return;
+		}
+
+		// Fake event.
+		const e = {
+			target: {
+				dataset: { name },
+				type: 'text',
+				value
+			}
+		};
+		update( e );
+	}, [] );
+
+	return (
+		<div className="mb-cpt-field">
+			<label className="mb-cpt-label" htmlFor={ name }>
+				{ label }
+				{ required && <span className="mb-cpt-required">*</span> }
+			</label>
+			<div className="mb-cpt-input">
+				<input type="text" required={ required } id={ name } data-name={ name } value={ value } onChange={ update } />
+				{ description && <div className="mb-cpt-description">{ description }</div> }
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Input;
