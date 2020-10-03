ac88206b9c4f0a60da3957f82d3d8d6907b26c6c
Anh Tran <anhtnt@elightup.com>
2020-10-03 10:50:51 +0700

    Fix empty page when clicking Get PHP Code tab

--- app/code.js
+++ app/code.js
@@ -1,16 +1,18 @@
+import dotProp from 'dot-prop';
+
 const maxKeyLengh = object => Math.max.apply( null, Object.keys( object ).map( key => key.length ) );
 const spaces = ( settings, key ) => ' '.repeat( maxKeyLengh( settings ) - key.length );
 
-const text = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => '${ settings[ key ] }'`;
-const translatableText = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => esc_html__( '${ settings[ key ] }', '${ settings.text_domain }' )`;
-const checkboxList = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => ${ settings[ key ].length ? `['${ settings[ key ].join( "', '" ) }']` : '[]' }`;
-const general = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => ${ settings[ key ] }`;
+const text = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => '${ dotProp.get( settings, key, '' ) }'`;
+const translatableText = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => esc_html__( '${ dotProp.get( settings, key, '' ) }', '${ settings.text_domain }' )`;
+const checkboxList = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => ${ dotProp.get( settings, key, [] ).length ? `['${ dotProp.get( settings, key, [] ).join( "', '" ) }']` : '[]' }`;
+const general = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => ${ dotProp.get( settings, key ) }`;
 
 const labels = settings => {
     const { labels } = settings;
 
     let keys = Object.keys( labels );
-    labels.text_domain = settings.text_domain; // Add text domain to run the `text` function above.
+    labels.text_domain = dotProp.get( settings, 'text_domain', 'your-textdomain' ); // Add text domain to run the `text` function above.
 
     return keys.map( key => translatableText( labels, key ) ).join( ",\n\t\t" );
 };
