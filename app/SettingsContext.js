2c85a0e1f115d8f16e28e9bf8bfd8ec78bce6093
Anh Tran <anhtnt@elightup.com>
2020-11-05 09:45:01 +0700

    Fix updateSettings

--- app/SettingsContext.js
+++ app/SettingsContext.js
@@ -4,7 +4,7 @@ export const SettingsContext = createContext();
 
 export const SettingsProvider = ( { children, value } ) => {
 	const [ settings, setSettings ] = useState( value );
-	const updateSettings = data => setSettings( prev => ( { ...prev, data } ) );
+	const updateSettings = data => setSettings( prev => ( { ...prev, ...data } ) );
 
 	return <SettingsContext.Provider value={ { settings, updateSettings } }>{ children }</SettingsContext.Provider>;
 };
