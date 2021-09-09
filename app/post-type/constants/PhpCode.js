ba237446db3091a084f8b08c482596dc8e34b756
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2021-09-09 11:08:53 +0700

    fix bug generated code not working

--- app/post-type/constants/PhpCode.js
+++ app/post-type/constants/PhpCode.js
@@ -1,7 +1,7 @@
 import { checkboxList, general, labels, spaces, text, translatableText } from '../../code';
 
 const advanced = settings => {
-	const ignore = [ 'slug', 'function_name', 'text_domain', 'label', 'labels', 'description', 'rest_base', 'show_in_menu', 'menu_icon', 'menu_position', 'capability_type', 'has_archive', 'archive_slug', 'rewrite', 'supports', 'taxonomies' ];
+	const ignore = [ 'slug', 'function_name', 'text_domain', 'label', 'labels', 'description', 'rest_base', 'show_in_menu', 'menu_icon', 'menu_position', 'capability_type', 'has_archive', 'archive_slug', 'rewrite', 'supports', 'taxonomies','icon_type','icon','icon_svg','icon_custom' ];
 
 	let keys = Object.keys( settings ).filter( key => !ignore.includes( key ) );
 	return keys.map( key => general( settings, key ) ).join( ",\n\t\t" );
@@ -13,10 +13,25 @@ const showInMenu = settings => {
 	let code = `'show_in_menu'${ spaces( settings, 'show_in_menu' ) } => ${ value },`;
 	if ( value === true ) {
 		code += `
-		${ text( settings, 'menu_icon' ) },
 		${ general( settings, 'menu_position' ) },`;
 	}
 	return code;
+};
+
+const menu_icon = settings => {
+	let value_type = settings.icon_type ? `'${ settings.icon_type }'` : settings.icon_type;
+	let value = settings.icon ? `'${ settings.icon }'` : settings.icon;
+
+	if( value_type == `'dashicons'` ){
+	   value = settings.icon ? `'${ settings.icon }'` : settings.icon;
+    }else if( value_type == `'svg'` ){
+       value = settings.icon_svg ? `'${ settings.icon_svg }'` : settings.icon_svg;
+    }else if( value_type == `'custom'` ){
+       value = settings.icon_custom ? `'${ settings.icon_custom }'` : settings.icon_custom;
+    }
+
+    return `'menu_icon'${ spaces( settings, 'menu_icon' ) } => ${ value }`;
+
 };
 
 const archive = settings => {
@@ -39,7 +54,7 @@ const rewrite = settings => {
 const PhpCode = settings => {
 	return `<?php
 add_action( 'init', '${ settings.function_name }' );
-function ${ settings.function_name }() {
+function ${ settings.function_name}() {
 	$labels = [
 		${ labels( settings ) },
 	];
@@ -51,6 +66,7 @@ function ${ settings.function_name }() {
 		${ archive( settings ) },
 		${ text( settings, 'rest_base' ) },
 		${ showInMenu( settings ) }
+		${ menu_icon( settings ) }
 		${ text( settings, 'capability_type' ) },
 		${ checkboxList( settings, 'supports', false ) },
 		${ checkboxList( settings, 'taxonomies', '[]' ) },
