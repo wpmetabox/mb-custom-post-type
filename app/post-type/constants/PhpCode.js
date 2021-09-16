64e9f37c2bbe0f9f72dfd5ac18ce75874bc8034f
Anh Tran <anhtnt@elightup.com>
2021-09-16 13:59:38 +0700

    Fix missing comma in exported code. Closes #38.

--- app/post-type/constants/PhpCode.js
+++ app/post-type/constants/PhpCode.js
@@ -1,7 +1,7 @@
 import { checkboxList, general, labels, spaces, text, translatableText } from '../../code';
 
 const advanced = settings => {
-	const ignore = [ 'slug', 'function_name', 'text_domain', 'label', 'labels', 'description', 'rest_base', 'show_in_menu', 'menu_icon', 'menu_position', 'capability_type', 'has_archive', 'archive_slug', 'rewrite', 'supports', 'taxonomies','icon_type','icon','icon_svg','icon_custom' ];
+	const ignore = [ 'slug', 'function_name', 'text_domain', 'label', 'labels', 'description', 'rest_base', 'show_in_menu', 'menu_icon', 'menu_position', 'capability_type', 'has_archive', 'archive_slug', 'rewrite', 'supports', 'taxonomies', 'icon_type', 'icon', 'icon_svg', 'icon_custom' ];
 
 	let keys = Object.keys( settings ).filter( key => !ignore.includes( key ) );
 	return keys.map( key => general( settings, key ) ).join( ",\n\t\t" );
@@ -12,8 +12,7 @@ const showInMenu = settings => {
 	value = [ true, false ].includes( value ) ? value : `'${ value }'`;
 	let code = `'show_in_menu'${ spaces( settings, 'show_in_menu' ) } => ${ value },`;
 	if ( value === true ) {
-		code += `
-		${ general( settings, 'menu_position' ) },`;
+		code += `${ general( settings, 'menu_position' ) },`;
 	}
 	return code;
 };
@@ -22,15 +21,15 @@ const menu_icon = settings => {
 	let value_type = settings.icon_type ? `'${ settings.icon_type }'` : settings.icon_type;
 	let value = settings.icon ? `'${ settings.icon }'` : settings.icon;
 
-	if( value_type == `'dashicons'` ){
-	   value = settings.icon ? `'${ settings.icon }'` : settings.icon;
-    }else if( value_type == `'svg'` ){
-       value = settings.icon_svg ? `'${ settings.icon_svg }'` : settings.icon_svg;
-    }else if( value_type == `'custom'` ){
-       value = settings.icon_custom ? `'${ settings.icon_custom }'` : settings.icon_custom;
-    }
+	if ( value_type == `'dashicons'` ) {
+		value = settings.icon ? `'${ settings.icon }'` : settings.icon;
+	} else if ( value_type == `'svg'` ) {
+		value = settings.icon_svg ? `'${ settings.icon_svg }'` : settings.icon_svg;
+	} else if ( value_type == `'custom'` ) {
+		value = settings.icon_custom ? `'${ settings.icon_custom }'` : settings.icon_custom;
+	}
 
-    return `'menu_icon'${ spaces( settings, 'menu_icon' ) } => ${ value }`;
+	return `'menu_icon'${ spaces( settings, 'menu_icon' ) } => ${ value }`;
 
 };
 
@@ -54,7 +53,7 @@ const rewrite = settings => {
 const PhpCode = settings => {
 	return `<?php
 add_action( 'init', '${ settings.function_name }' );
-function ${ settings.function_name}() {
+function ${ settings.function_name }() {
 	$labels = [
 		${ labels( settings ) },
 	];
@@ -66,7 +65,7 @@ function ${ settings.function_name}() {
 		${ archive( settings ) },
 		${ text( settings, 'rest_base' ) },
 		${ showInMenu( settings ) }
-		${ menu_icon( settings ) }
+		${ menu_icon( settings ) },
 		${ text( settings, 'capability_type' ) },
 		${ checkboxList( settings, 'supports', false ) },
 		${ checkboxList( settings, 'taxonomies', '[]' ) },
