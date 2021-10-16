a0c22dbe3ee0f2b0cf877151e860ff82e1784963
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2021-10-16 14:32:01 +0700

    fix/simple quote make wrong code in generator

--- app/code.js
+++ app/code.js
@@ -2,9 +2,12 @@ import dotProp from 'dot-prop';
 
 const maxKeyLength = object => Math.max.apply( null, Object.keys( object ).map( key => key.length ) );
 const spaces = ( settings, key ) => ' '.repeat( maxKeyLength( settings ) - key.length );
-
-const text = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => '${ dotProp.get( settings, key, '' ) }'`;
-const translatableText = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => esc_html__( "${ dotProp.get( settings, key, '' ) }", '${ settings.text_domain }' )`;
+const checktext = ( settings, key ) => {
+    let checktext = dotProp.get( settings, key, '' ).replace(/\'/g, '\\\'');
+    return checktext;
+};
+const text = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => '${ checktext( settings, key ) }'`;
+const translatableText = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => esc_html__( '${ checktext( settings, key ) }', '${ settings.text_domain }' )`;
 const checkboxList = ( settings, key, defaultValue ) => `'${ key }'${ spaces( settings, key ) } => ${ dotProp.get( settings, key, [] ).length ? `['${ dotProp.get( settings, key, [] ).join( "', '" ) }']` : defaultValue }`;
 const general = ( settings, key ) => {
     let value = dotProp.get( settings, key );
