d52e25257538fbc8ba4cab526efbadb95208e7fb
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2021-10-20 14:49:28 +0700

    fix tên biến

--- app/code.js
+++ app/code.js
@@ -2,13 +2,13 @@ import dotProp from 'dot-prop';
 
 const maxKeyLength = object => Math.max.apply( null, Object.keys( object ).map( key => key.length ) );
 const spaces = ( settings, key ) => ' '.repeat( maxKeyLength( settings ) - key.length );
-const checktext = ( settings, key ) => {
-    let checktext = dotProp.get( settings, key, '' ).replace(/\\/g, '\\\\');
-        checktext = checktext.replace(/\'/g, '\\\'');
-    return checktext;
+const checkText = ( settings, key ) => {
+    let value = dotProp.get( settings, key, '' ).replace(/\\/g, '\\\\');
+        value = value.replace(/\'/g, '\\\'');
+    return value;
 };
-const text = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => '${ checktext( settings, key ) }'`;
-const translatableText = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => esc_html__( '${ checktext( settings, key ) }', '${ settings.text_domain }' )`;
+const text = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => '${ checkText( settings, key ) }'`;
+const translatableText = ( settings, key ) => `'${ key }'${ spaces( settings, key ) } => esc_html__( '${ checkText( settings, key ) }', '${ settings.text_domain }' )`;
 const checkboxList = ( settings, key, defaultValue ) => `'${ key }'${ spaces( settings, key ) } => ${ dotProp.get( settings, key, [] ).length ? `['${ dotProp.get( settings, key, [] ).join( "', '" ) }']` : defaultValue }`;
 const general = ( settings, key ) => {
     let value = dotProp.get( settings, key );
