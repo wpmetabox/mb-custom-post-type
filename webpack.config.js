9a78b25424af2f543e573b47be6df75660e6bfbe
Anh Tran <anhtnt@elightup.com>
2022-10-15 10:44:18 +0700

    Update Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,46 @@
 const path = require( 'path' );
-const TerserPlugin = require( "terser-webpack-plugin" );
 
-// https://www.cssigniter.com/how-to-use-external-react-components-in-your-gutenberg-blocks/
-const externals = {
-	'react': 'React',
-	'react-dom': 'ReactDOM',
-	'codemirror': 'wp.CodeMirror',
-	'clipboard': 'ClipboardJS',
-	'@wordpress/i18n': 'wp.i18n',
-	'@wordpress/element': 'wp.element',
-};
-
-const commonModules = {
-	rules: [
-		{
-			test: /\.js/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					plugins: [ '@babel/plugin-transform-react-jsx' ]
+const config = {
+	externals: {
+		'react': 'React',
+		'react-dom': 'ReactDOM',
+		'codemirror': 'wp.CodeMirror',
+		'clipboard': 'ClipboardJS',
+		'@wordpress/i18n': 'wp.i18n',
+		'@wordpress/element': 'wp.element',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						plugins: [ '@babel/plugin-transform-react-jsx' ]
+					}
 				}
-			}
-		},
-	]
+			},
+		]
+	}
 };
 
-const CptConfig = {
+const postType = {
+	...config,
 	entry: './app/post-type/App.js',
 	output: {
 		filename: 'post-type.js',
 		path: path.resolve( __dirname, './assets' )
 	},
-	externals,
-	module: commonModules
 };
 
-const TaxonomyConfig = {
+const taxonomy = {
+	...config,
 	entry: './app/taxonomy/App.js',
 	output: {
 		filename: 'taxonomy.js',
 		path: path.resolve( __dirname, './assets' )
 	},
-	externals,
-	module: commonModules
 };
 
-module.exports = [ CptConfig, TaxonomyConfig ];
+module.exports = [ postType, taxonomy ];
