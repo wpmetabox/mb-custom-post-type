486615825553f41e9c7c2255c3e8e79b60810081
Bao <lexuanbao161096@gmail.com>
2023-01-11 17:30:56 +0700

    Show icon selected

--- app/controls/Icon.js
+++ app/controls/Icon.js
@@ -1,16 +1,27 @@
-const { Dashicon } = wp.components;
-const { useState } = wp.element;
 import Tooltip from './Tooltip';
+import { useState } from "@wordpress/element";
 
 const Icon = ( { label, name, update, value, required = false, tooltip = '' } ) => {
 	const [ query, setQuery ] = useState( "" );
-	const keyLabel = Object.entries( MBCPT.fullIcons ).find( ( [ key, label ] ) => `dashicons-${ key }` === value );
-	const data = Object.entries( MBCPT.fullIcons ).filter( ( [ key, label ] ) => {
-		if ( `dashicons-${ key }` !== value ) {
-			return [ key, label ];
-		}
-	} );
-	data.unshift( keyLabel );
+	let data = MBCPT.icons.map( icon => {
+			let label = icon;
+			if ( icon.includes( '-' ) ) {
+				label = icon.replace( /-/g, ' ' );
+			}
+			let startsText = [ 'admin', 'controls', 'editor', 'format', 'image', 'welcom' ];
+			for ( let text in startsText ) {
+				if ( icon.startsWith( text ) ) {
+					label = label.replace( text, '' );
+				}
+			}
+			let endsText = [ 'alt', 'alt2', 'alt3' ];
+			for ( let text in endsText ) {
+				if ( icon.endsWith( text ) ) {
+					label = label.replace( text, `(${ text })` );
+				}
+			}
+			return [ icon, label ];
+		} );
 
 	return (
 		<div className="mb-cpt-field mb-cpt-field--radio">
@@ -20,10 +31,13 @@ const Icon = ( { label, name, update, value, required = false, tooltip = '' } ) 
 				{ tooltip && <Tooltip id={ name } content={ tooltip } /> }
 			</label>
 			<div className='mb-cpt-input'>
-				<input type="text" className="mb-cpt-search" placeholder="Search..." value={ query } onChange={ event => setQuery( event.target.value ) } />
+				<div className='mb-cpt-icon-selected'>
+					<span className={ `dashicons ${ value }` }></span>
+					<input type="text" className="mb-cpt-search" placeholder="Search..." value={ query } onChange={ event => setQuery( event.target.value ) } />
+				</div>
 				<div className="mb-cpt-items">
 					{
-						Object.entries( MBCPT.fullIcons ).filter( ( [ key, label ] ) => {
+						data.filter( ( [ key, label ] ) => {
 							if ( query === '' ) {
 								return [ key, label ];
 							} else if ( label.toLowerCase().includes( query.toLowerCase() ) ) {
@@ -33,7 +47,7 @@ const Icon = ( { label, name, update, value, required = false, tooltip = '' } ) 
 							<div className='mb-cpt-item'>
 								<label key={ key } className="mb-cpt-icon">
 									<input type="radio" name={ name } value={ `dashicons-${ key }` } checked={ `dashicons-${ key }` === value } onChange={ update } />
-									<Dashicon icon={ key } />
+									<span className={ `dashicons dashicons-${ key }` }></span>
 								</label>
 								<span className='mb-cpt-item__text'>{ label }</span>
 							</div>
