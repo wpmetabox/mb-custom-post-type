d17b702b6477e1138936d6dd77fc0ef30ce3fa40
Bao <lexuanbao161096@gmail.com>
2023-01-14 10:13:33 +0700

    Update description

--- app/controls/Fontawesome.js
+++ app/controls/Fontawesome.js
@@ -1,6 +1,7 @@
 import Tooltip from './Tooltip';
+import { RawHTML } from '@wordpress/element';
 
-const Fontawesome = ( { label, name, update, value, required = false, tooltip = '' } ) => {
+const Fontawesome = ( { label, name, update, value, required = false, tooltip = '', description = '' } ) => {
 	return (
 		<div className="mb-cpt-field">
 			<label className="mb-cpt-label" htmlFor={ name }>
@@ -8,9 +9,12 @@ const Fontawesome = ( { label, name, update, value, required = false, tooltip = 
 				{ required && <span className="mb-cpt-required">*</span> }
 				{ tooltip && <Tooltip id={ name } content={ tooltip } /> }
 			</label>
-			<div className='mb-cpt-input mb-cpt-icon-selected'>
-				<span className={ `icon-fontawesome ${ value }` }></span>
-				<input type="text" name={ name } value={ value } onChange={ update } />
+			<div className='mb-cpt-input'>
+				<div className='mb-cpt-icon-selected'>
+					<span className={ `icon-fontawesome ${ value }` }></span>
+					<input type="text" name={ name } value={ value } onChange={ update } />
+				</div>
+				{ description && <div className="mb-cpt-description"><RawHTML>{ description }</RawHTML></div> }
 			</div>
 		</div>
 	);
