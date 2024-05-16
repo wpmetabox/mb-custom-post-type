b074d080f9d578abe8782b9847d1ce294be59c67
quyennv <vanquyen2703ht@gmail.com>
2024-05-16 14:25:00 +0700

    update fix alert field slug when exceed 20 characters

--- app/controls/Slug.js
+++ app/controls/Slug.js
@@ -1,13 +1,31 @@
-import { RawHTML, useEffect } from '@wordpress/element';
+import { RawHTML,  useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import Tooltip from './Tooltip';
 
-const Slug = ( { label, name, value, update, tooltip = '', description = '', required = false } ) => {
-	const error = MBCPT.reservedTerms.includes( value ) ? __( 'ERROR: the slug must not be one of WordPress <a href="https://codex.wordpress.org/Reserved_Terms" target="_blank" rel="noopenner noreferrer">reserved terms</a>', 'mb-custom-post-type' ) : null;
+const Slug = ({ label, name, initialValue = '', tooltip = '', description = '', required = false }) => {
+	const [value, setValue] = useState(initialValue);
+    const isReservedTerm = MBCPT.reservedTerms.includes(value);
+    const isTooLong = value.length > 20;
+    const error = isReservedTerm
+        ? __('Q-ERROR: the slug must not be one of WordPress <a href="https://codex.wordpress.org/Reserved_Terms" target="_blank" rel="noopener noreferrer">reserved terms</a>', 'mb-custom-post-type')
+        : isTooLong
+        ? sprintf(__('ERROR: the slug must not exceed %d characters.', 'mb-custom-post-type'), 20)
+        : null;
 
-	useEffect( () => {
-		document.querySelector( '#publish' ).disabled = MBCPT.reservedTerms.includes( value );
-	}, [ value ] );
+    useEffect(() => {
+        const publishButton = document.querySelector('#publish');
+        if (publishButton) {
+            publishButton.disabled = !!error;
+        }
+    }, [error]);
+
+    const handleChange = (e) => {
+		const newValue = e.target.value;
+        console.log('Value:', newValue);
+        console.log('Value Length:', newValue.length);
+        setValue(newValue);
+        // setValue(e.target.value);
+    };
 
 	return (
 		<div className="mb-cpt-field">
@@ -17,7 +35,7 @@ const Slug = ( { label, name, value, update, tooltip = '', description = '', req
 				{ tooltip && <Tooltip id={ name } content={ tooltip } /> }
 			</label>
 			<div className="mb-cpt-input">
-				<input type="text" required={ required } id={ name } name={ name } value={ value } onChange={ update } />
+				<input type="text" required={ required } id={ name } name={ name } value={ value } onChange={handleChange} />
 				{ description && <div className="mb-cpt-description">{ description }</div> }
 				{ error && <RawHTML className="mb-cpt-error">{ error }</RawHTML> }
 			</div>
