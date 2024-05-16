0b8676f594015670ea9e42aa4a99cc5d77d22822
quyennv <vanquyen2703ht@gmail.com>
2024-05-16 15:37:38 +0700

    update show alert error when exceed 20 character slug

--- app/controls/Slug.js
+++ app/controls/Slug.js
@@ -1,31 +1,19 @@
-import { RawHTML,  useState, useEffect } from '@wordpress/element';
+import { RawHTML, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import Tooltip from './Tooltip';
 
-const Slug = ({ label, name, initialValue = '', tooltip = '', description = '', required = false }) => {
-	const [value, setValue] = useState(initialValue);
-    const isReservedTerm = MBCPT.reservedTerms.includes(value);
-    const isTooLong = value.length > 20;
-    const error = isReservedTerm
-        ? __('ERROR: the slug must not be one of WordPress <a href="https://codex.wordpress.org/Reserved_Terms" target="_blank" rel="noopener noreferrer">reserved terms</a>', 'mb-custom-post-type')
-        : isTooLong
-        ? sprintf(__('ERROR: the slug must not exceed %d characters.', 'mb-custom-post-type'), 20)
-        : null;
+const Slug = ( { label, name, value, update, tooltip = '', description = '', required = false } ) => {
+	const isReservedTerm = MBCPT.reservedTerms.includes( value );
+	const isTooLong = value.length > 20;
+	const error = isReservedTerm
+		? __( 'ERROR: the slug must not be one of WordPress <a href="https://codex.wordpress.org/Reserved_Terms" target="_blank" rel="noopener noreferrer">reserved terms</a>', 'mb-custom-post-type' )
+		: isTooLong
+			? sprintf( __( 'ERROR: the slug must not exceed %d characters.', 'mb-custom-post-type' ), 20 )
+			: '';
 
-    useEffect(() => {
-        const publishButton = document.querySelector('#publish');
-        if (publishButton) {
-            publishButton.disabled = !!error;
-        }
-    }, [error]);
-
-    const handleChange = (e) => {
-		const newValue = e.target.value;
-        console.log('Value:', newValue);
-        console.log('Value Length:', newValue.length);
-        setValue(newValue);
-        // setValue(e.target.value);
-    };
+	useEffect( () => {
+		document.querySelector( '#publish' ).disabled = !!error;
+	}, [ error ] );
 
 	return (
 		<div className="mb-cpt-field">
@@ -35,7 +23,7 @@ const Slug = ({ label, name, initialValue = '', tooltip = '', description = '', 
 				{ tooltip && <Tooltip id={ name } content={ tooltip } /> }
 			</label>
 			<div className="mb-cpt-input">
-				<input type="text" required={ required } id={ name } name={ name } value={ value } onChange={handleChange} />
+				<input type="text" required={ required } id={ name } name={ name } value={ value } onChange={ update } />
 				{ description && <div className="mb-cpt-description">{ description }</div> }
 				{ error && <RawHTML className="mb-cpt-error">{ error }</RawHTML> }
 			</div>
