546740fbc97a01ab6a15b26575ea5a43bb28ac95
Anh Tran <anhtnt@elightup.com>
2024-09-16 11:31:48 +0700

    Ensure set initial orders run only on the edit screen

--- src/Order.php
+++ src/Order.php
@@ -5,31 +5,35 @@ use WP_Query;
 
 class Order {
 	public function __construct() {
-		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_assets' ] );
+		add_action( 'load-edit.php', [ $this, 'setup_for_edit_screen' ] );
 		add_action( 'wp_ajax_mbcpt_update_menu_order', [ $this, 'update_menu_order' ] );
-		add_action( 'pre_get_posts', [ $this, 'order_pre_get_posts' ] );
+		add_action( 'pre_get_posts', [ $this, 'set_orderby_menu_order' ] );
 		add_filter( 'get_previous_post_where', [ $this, 'order_previous_post_where' ] );
 		add_filter( 'get_previous_post_sort', [ $this, 'order_previous_post_sort' ] );
 		add_filter( 'get_next_post_where', [ $this, 'order_next_post_where' ] );
 		add_filter( 'get_next_post_sort', [ $this, 'order_next_post_sort' ] );
 	}
 
-	public function enqueue_assets(): void {
+	public function setup_for_edit_screen(): void {
 		$screen = get_current_screen();
 		if ( $screen->base !== 'edit' || ! $this->is_enabled_ordering( $screen->post_type ) ) {
 			return;
 		}
 
+		// Add admin columns
+		add_filter( "manage_{$screen->post_type}_posts_columns", [ $this, 'add_admin_order_column' ] );
+		add_action( "manage_{$screen->post_type}_posts_custom_column", [ $this, 'show_admin_order_column' ] );
+
+		// Set initial orders
+		$this->set_initial_orders( $screen->post_type );
+
+		// Enqueue assets
 		wp_enqueue_style( 'order', MB_CPT_URL . 'assets/order.css', [], MB_CPT_VER );
 		wp_enqueue_script( 'order', MB_CPT_URL . 'assets/order.js', [ 'jquery-ui-sortable' ], MB_CPT_VER, true );
 		wp_localize_script( 'order', 'MBCPT', [ 'security' => wp_create_nonce( 'order' ) ] );
 	}
 
 	private function set_initial_orders( string $post_type ): void {
-		if ( $this->is_doing_ajax() ) {
-			return;
-		}
-
 		global $wpdb;
 		$query = $wpdb->prepare(
 			"
@@ -96,17 +100,13 @@ class Order {
 		}
 	}
 
-	public function order_pre_get_posts( WP_Query $query ): void {
+	public function set_orderby_menu_order( WP_Query $query ): void {
 		$post_type = $query->get( 'post_type' );
 
 		if ( ! $post_type || ! is_string( $post_type ) || ! $this->is_enabled_ordering( $post_type ) ) {
 			return;
 		}
-		if ( is_admin() ) {
-			$this->set_initial_orders( $post_type );
-			add_filter( "manage_{$post_type}_posts_columns", [ $this, 'add_admin_order_column' ] );
-			add_action( "manage_{$post_type}_posts_custom_column", [ $this, 'show_admin_order_column' ] );
-		}
+
 		if ( $query->get( 'orderby' ) ) {
 			return;
 		}
@@ -166,17 +166,5 @@ class Order {
 	private function is_enabled_ordering( string $post_type ): bool {
 		$post_type_object = get_post_type_object( $post_type );
 		return ! empty( $post_type_object->order );
-	}
-
-	private function is_doing_ajax(): bool {
-		if ( function_exists( 'wp_doing_ajax' ) ) {
-			return wp_doing_ajax();
-		}
-
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			return true;
-		}
-
-		return false;
 	}
 }
