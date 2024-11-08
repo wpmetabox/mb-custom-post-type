d369bd82d095c42c72633328f5db80af1f75918b
Anh Tran <anhtnt@elightup.com>
2024-11-08 14:56:19 +0700

    Add built files

--- assets/build/taxonomy.asset.php
+++ assets/build/taxonomy.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4bbb9b41a1131fbf5022');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '349d300fdb84ab791136');
