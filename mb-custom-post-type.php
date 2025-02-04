c009b0c18ae11fc91f5f915a4db603e070267a93
Anh Tran <anhtnt@elightup.com>
2025-02-04 15:27:37 +0700

    Version 2.8.0

--- mb-custom-post-type.php
+++ mb-custom-post-type.php
@@ -3,11 +3,26 @@
  * Plugin Name: MB Custom Post Types & Custom Taxonomies
  * Plugin URI:  https://metabox.io/plugins/custom-post-type/
  * Description: Create custom post types and custom taxonomies with easy-to-use UI
- * Version:     2.7.10
+ * Version:     2.8.0
  * Author:      MetaBox.io
  * Author URI:  https://metabox.io
  * License:     GPL-2.0+
  * Text Domain: mb-custom-post-type
+ *
+ * Copyright (C) 2010-2025 Tran Ngoc Tuan Anh. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Prevent loading this file directly.
@@ -24,7 +39,7 @@ if ( ! function_exists( 'mb_cpt_load' ) ) {
 
 	function mb_cpt_load() {
 		define( 'MB_CPT_DIR', __DIR__ );
-		define( 'MB_CPT_VER', '2.7.10' );
+		define( 'MB_CPT_VER', '2.8.0' );
 
 		if ( class_exists( 'RWMB_Loader' ) ) {
 			list( , $url ) = RWMB_Loader::get_path( __DIR__ );
--- readme.txt
+++ readme.txt
@@ -3,9 +3,9 @@ Contributors: elightup, metabox, rilwis, duc88b, truongwp, barcavn2
 Donate link: https://metabox.io/pricing/
 Tags: custom post types, custom taxonomies, posts, taxonomies
 Requires at least: 5.9
-Tested up to: 6.7
+Tested up to: 6.7.1
 Requires PHP: 7.2
-Stable tag: 2.7.10
+Stable tag: 2.8.0
 License: GPLv2 or later
 
 Create and manage custom post types and custom taxonomies with an easy-to-use UI in WordPress.
@@ -60,6 +60,10 @@ If you like this plugin, you might also like our other WordPress products:
 1. Edit custom post type - Taxonomies tab
 
 == Changelog ==
+
+= 2.8.0 - 2025-02-04 =
+- Add custom permissions for taxonomy
+- Fix saving issues on Safari
 
 = 2.7.10 - 2024-11-19 =
 - Improve accessibility of the switch control
