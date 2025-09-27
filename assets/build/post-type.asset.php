7cbb7d8d41f5149f5f87874d4842e7d63e35f5b7
Anh Tran <anhtnt@elightup.com>
2025-09-27 09:52:54 +0700

    Build

--- assets/build/post-type.asset.php
+++ assets/build/post-type.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9839978b20b789442416');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ee03718b0c5005985a65');
