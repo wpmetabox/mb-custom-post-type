2e0cd20e2791c4ac0413c0678a2798995460ee72
Hoang Minh Huong <huongcodecb1992@gmail.com>
2025-10-07 09:02:07 +0700

    feat: add text domain support for label translations

--- assets/build/post-type.asset.php
+++ assets/build/post-type.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ee03718b0c5005985a65');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b52b304731c331f75d50');
--- assets/build/taxonomy.asset.php
+++ assets/build/taxonomy.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f578f7ee0bfe6d1844d8');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6772ef200626aee5c69f');
