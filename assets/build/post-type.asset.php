dc04c1f919fa586c87d962ca0b0f4694428ca920
Anh Tran <anhtnt@elightup.com>
2026-02-25 17:41:01 +0700

    Build

--- assets/build/post-type.asset.php
+++ assets/build/post-type.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'faea4ab4789000cb4ccd');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a4f1433218b5a75fd845');
--- assets/build/taxonomy.asset.php
+++ assets/build/taxonomy.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '41ebc1fca312daa6e13c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '06c2b560436df3d2cf55');
