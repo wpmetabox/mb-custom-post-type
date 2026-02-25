afe7f651f55414383b0704524006817f9915a39f
huongcodecb1992 <82349955+huongcodecb1992@users.noreply.github.com>
2026-02-25 17:43:00 +0700

    add missing comma after capabilities in taxonomy PHP code output (#102)

--- assets/build/taxonomy.asset.php
+++ assets/build/taxonomy.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2118adbdba7b62c996cd');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '06c2b560436df3d2cf55');
